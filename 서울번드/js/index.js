b73d4b5a125a3a149353a80cde45fcb1d37e6a9d
jaeee <72128840+jaeeedev@users.noreply.github.com>
2022-07-08 17:48:15 +0900

    Add files via upload

--- 서울번드/js/index.js
+++ 서울번드/js/index.js
@@ -36,10 +36,118 @@ const swiper = new Swiper(".swiper1", {
   },
 });
 
-new Swiper(".swiper2", {
+//이달의 ~~
+new Swiper(".swiper_new", {
   loop: true,
+  autoplay: true,
   slidesPerView: 2,
-  pagination: {
-    el: ".swiper-pagination",
+  spaceBetween: 10,
+
+  breakpoints: {
+    510: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
   },
 });
+
+new Swiper(".swiper_best", {
+  loop: true,
+  autoplay: true,
+  slidesPerView: 2,
+  spaceBetween: 10,
+
+  breakpoints: {
+    510: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+const monthlyBtns = document.querySelector(".product_btns");
+monthlyBtns.addEventListener("click", (e) => {
+  const newBtn = monthlyBtns.querySelector(".product_new");
+  const bestBtn = monthlyBtns.querySelector(".product_best");
+  const newSlide = document.querySelector(".swiper_new");
+  const bestSlide = document.querySelector(".swiper_best");
+
+  if (e.target === newBtn) {
+    newSlide.classList.add("active");
+    bestSlide.classList.remove("active");
+    newBtn.classList.add("active");
+    bestBtn.classList.remove("active");
+  } else if (e.target === bestBtn) {
+    bestSlide.classList.add("active");
+    newSlide.classList.remove("active");
+    bestBtn.classList.add("active");
+    newBtn.classList.remove("active");
+  }
+});
+
+//작가
+
+const artistPdBox = document.querySelector(".artist_product");
+console.log(window.getComputedStyle(artistPdBox).height);
+
+//번더의 취향
+
+new Swiper(".swiper_wellness", {
+  loop: true,
+  autoplay: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+new Swiper(".swiper_kitchen", {
+  loop: true,
+  autoplay: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+//공지사항 슬라이드
+
+const eventSlide = document.querySelector(".notice_slide");
+const slideNodes = document.querySelectorAll(".notice_slide .slide");
+const cloneFirst = slideNodes[0].cloneNode(true);
+const cloneLast = slideNodes[slideNodes.length - 1].cloneNode(true);
+let currentMove = 0;
+eventSlide.append(cloneFirst);
+eventSlide.insertBefore(cloneLast, slideNodes[0]);
+
+console.log(currentMove);
+
+setInterval(() => {
+  currentMove += 60;
+  eventSlide.style.transform = `translateY(-${currentMove}px)`;
+  eventSlide.style.transition = "all 2s 1s";
+
+  if (currentMove >= 300) {
+    setTimeout(() => {
+      eventSlide.style.transition = "none";
+      currentMove = 0;
+      eventSlide.style.transform = `translateY(-${currentMove})`;
+    }, 2900);
+  }
+}, 3000);
