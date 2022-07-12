41ba75f68efccabfdaddf73c9c9e96450f0c3e23
jaeee <72128840+jaeeedev@users.noreply.github.com>
2022-07-12 23:23:00 +0900

    Add files via upload

--- 서울번드/js/detail.js
+++ 서울번드/js/detail.js
@@ -81,18 +81,22 @@ let orderItems = [
     option: "오렌지",
     price: 28000,
     count: 1,
+    id: "orange",
   },
   {
     option: "핑크 (+3000)",
     price: 31000,
     count: 1,
+    id: "pink",
   },
   {
     option: "블루",
     price: 28000,
     count: 1,
+    id: "blue",
   },
 ];
+
 let set = new Set();
 let orderOptions = [];
 let totalAmount = 0;
@@ -109,7 +113,7 @@ selectOption.innerHTML = options;
 
 function addItem() {
   const index = selectOption.options.selectedIndex - 1; //option 제일 첫 요소가 placeholder용이라서 제외
-  set.add(orderItems[index]); //중복 방지용
+  set.add(orderItems[index]);
   orderOptions = [...set];
   if (orderItems.length === orderOptions.length) return;
 }
@@ -124,7 +128,7 @@ function writeItem() {
   const totalBoxContents =
     orderOptions
       .map((item) => {
-        return `<div class="total_item">
+        return `<div class="total_item" data-id=${item.id}>
       <span class="item_name"
       >[서울번드] WGNB PULL GLASS CUP 풀 고블렛 유리컵</span
     >
@@ -132,7 +136,7 @@ function writeItem() {
       <span class="item_option">- ${item.option}</span>
       <div>
         <span class="data-price">${item.price}</span>
-        <span class="delete_item"
+        <span class="delete_item" data-id=${item.id}
           ><i class="fa-solid fa-xmark"></i
         ></span>
       </div>
@@ -146,6 +150,7 @@ function writeItem() {
         min="1"
         max="10"
         value="${item.count}"
+        data-id=${item.id}
       />
       <button class="quantity_plus">+</button>
     </div>
@@ -159,6 +164,32 @@ function writeItem() {
 
   const totalBox = document.querySelector(".total_box");
   totalBox.innerHTML = totalBoxContents;
+
+  totalItems = document.querySelectorAll(".total_item");
+
+  totalItems.forEach((box, i) => {
+    box.addEventListener("click", (e) => {
+      const substract = box.querySelector(".quantity_substract");
+      const plus = box.querySelector(".quantity_plus");
+      const countInput = box.querySelector(".current_quantity");
+      const thisTotalPrice = box.querySelector(".data-price");
+      const deleteBtn = box.querySelector(".delete_item i");
+
+      if (e.target === deleteBtn) {
+        console.log("click");
+        deleteItem(e, i);
+      } else if (e.target === plus) {
+        if (countInput.value >= 10) return;
+        countInput.value++;
+        //input 의 value는 orderoptions의 count값으로 렌더링되고있어서 count 잡아줘야하는데,,, 참조 타입이라 삭제할때 골치라 어떻게 짜야할지가 고민
+        //삭제할때 카운트 초기화시켜
+        orderOptions[i].count++;
+      } else if (e.target === substract) {
+        if (countInput.value <= 1) return;
+        countInput.value--;
+      }
+    });
+  });
 }
 let totalItems;
 
@@ -170,25 +201,22 @@ selectOption.addEventListener("change", () => {
   addItem();
   writeItem();
   calcTotal();
-
-  totalItems = document.querySelectorAll(".total_item");
-
-  totalItems.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      const substract = btn.querySelector(".quantity_substract");
-      const plus = btn.querySelector(".quantity_plus");
-      const countInput = btn.querySelector(".current_quantity");
-      const thisTotalPrice = btn.querySelector(".data-price");
-      const deleteBtn = btn.querySelector(".delete_item i");
-    });
-  });
 });
 
 //변화 발생하면 -> 변동사항 발생하고 -> 총계 연산하고 -> 다시 화면에 그려짐(변화의 경우 -> input 수량 변경 / 요소 삭제)
 
 function changeAmount() {}
 
-function deleteItem(e) {}
+function deleteItem(e, i) {
+  const filter = orderOptions.filter(
+    (item) => item.id !== e.target.parentElement.dataset.id
+  );
+  set.delete(orderOptions[i]);
+  orderOptions[i].count = 1; //참조형이라서 초기화해줘야함
+  orderOptions = filter;
+  writeItem();
+  calcTotal();
+}
 
 //상품 후기
 
@@ -196,7 +224,7 @@ function deleteItem(e) {}
 
 function readURL(input) {
   if (input.files && input.files[0]) {
-    console.log(input.files);
+    // console.log(input.files);
     var reader = new FileReader();
     reader.onload = function (e) {
       document.querySelector(
@@ -210,6 +238,29 @@ function readURL(input) {
     document.querySelector(".preview").src = "";
   }
 }
+
+async function fetchData() {
+  await fetch("https://shop-aac53-default-rtdb.firebaseio.com/orders.json", {
+    method: "POST",
+    body: JSON.stringify({
+      //imgSrc : 사진주소
+      //star: 별점
+      //
+      test: "Hi",
+    }),
+  });
+}
+
+async function fetchReview() {
+  const response = await fetch(
+    "https://shop-aac53-default-rtdb.firebaseio.com/orders.json"
+  );
+  const data = await response.json();
+  console.log(data);
+}
+
+// fetchData();
+// fetchReview();
 
 //리뷰 개수
 const reviews = document.querySelectorAll(".review_box");
